f2de8a94ef6502027564ab9935490954834caa62
stbere <taihmuhshuu@gmail.com>
2024-01-23 13:53:28 -0600

    Adding some simple, but helpful PS files. . .

---
+++ Add-Words-to-Beginning-of-AllFiles_InFolder.ps1
@@ -0,0 +1,8 @@
+﻿$folderPath = "C:\Data\Powershell\TestFiles"
+$files = Get-ChildItem $folderPath
+
+foreach ($file in $files) {
+$newName = "new_" + $file.Name
+$newPath = Join-Path -Path $file.Directory.FullName -ChildPath $newName
+Rename-Item -Path $file.FullName -NewName $newName
+}
---
+++ Add-Words-to-Beginning-of-AllFolders.ps1
@@ -0,0 +1,8 @@
+﻿$folderPath = "C:\Data\Powershell"
+$files = Get-ChildItem $folderPath
+
+foreach ($file in $files) {
+$newName = "new_" + $file.Name
+$newPath = Join-Path -Path $folderPath -ChildPath $newName
+Rename-Item -Path $file.FullName -NewName $newName
+}
