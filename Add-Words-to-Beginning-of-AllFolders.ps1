aa503245a1bea312fd1b98d32371e6c6fc06c187
stbere <taihmuhshuu@gmail.com>
2024-01-28 14:06:56 -0600

    Edits. . .

--- Add-Words-to-Beginning-of-AllFolders.ps1
+++ Add-Words-to-Beginning-of-AllFolders.ps1
@@ -2,7 +2,7 @@
 $files = Get-ChildItem $folderPath
 
 foreach ($file in $files) {
-$newName = "new_" + $file.Name # You can modify the `$newName` variable to suit your renaming requirements.
+$newName = "new_" + $file.Name # You can modify the `$newName` variable to suit your renaming requirements
 $newPath = Join-Path -Path $folderPath -ChildPath $newName
 Rename-Item -Path $file.FullName -NewName $newName
 }
